b5773bd34eda7219545ac00daf6e39e55449a120
Isaac Shivvers <ishivvers@gmail.com>
2017-07-09 15:41:16 -0700

    auto-update script updated for the modern era!

--- scripts/update_maps.sh
+++ scripts/update_maps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ~/.profile
 
+## Update the maps .json files
 ##########################################################################
 cd $WEBPAGEDIR/maps/scripts/
 
@@ -10,13 +11,14 @@ date >> updated.log
 python pull_sne_data.py && echo 'SN update success' >> updated.log
 python pull_grbs_data.py && echo 'GRB update success' >> updated.log
 
-git add -u
+git add ../js/sne.json ../js/grbs.json
 git commit -m 'SNe & GRB lists updated'
 git push
 
+## Update the website to point to the updated files
 ##########################################################################
 cd $WEBPAGEDIR/
 
-# git add -u
-# git commit -m 'Maps updated'
-# git push
+git add -u
+git commit -m 'Maps updated'
+git push
